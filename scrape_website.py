70689cc8fa9331f65945a3571c94d53c69cca780
miko103 <84585119+miko103@users.noreply.github.com>
2024-07-19 19:17:24 +0000

    started scraper

--- scrape_website.py
+++ scrape_website.py
@@ -1,2 +1,12 @@
 import requests
-# import 
+from bs4 import BeautifulSoup
+import os
+
+def get_links(url):
+    soup = BeautifulSoup(requests.get(url=url).text)
+    print(soup)
+
+
+
+url = "https://google.com"
+l = get_links(url)
