075e4df1145731c4cb6914848acec775d7e78e72
miki <84585119+miko1034@users.noreply.github.com>
2024-07-28 12:20:15 +0100

    added first time run script

---
+++ first_time.py
@@ -0,0 +1,27 @@
+import sqlite3
+import os
+
+USER_DB_NAME = "user.db"
+WEBSITE_DB_NAME = "website_data.db"
+
+if USER_DB_NAME not in os.listdir():
+    os.system(f'echo "" > {USER_DB_NAME}')
+    print("[+] user database created")
+    c = sqlite3.connect(USER_DB_NAME)
+    cursor = c.cursor()
+
+    # creates table for user data
+    cursor.execute("CREATE TABLE ud(username,phash,email,vercode)")
+    print("table ud (user data) created in user table")
+    c.close()
+    if WEBSITE_DB_NAME not in os.listdir():
+        os.system(f'echo "" > {WEBSITE_DB_NAME}')
+        print("[+] website database created")
+    else:
+        print(["[!] website database already exists"])
+        quit()
+
+else:
+    print("[!] user database already exists\n[!] exiting program...")
+    quit()
+
