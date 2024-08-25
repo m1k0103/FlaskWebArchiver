592e4431d9314f6deaac94157a5c59f879244f86
mikok <84585119+miko1034@users.noreply.github.com>
2024-08-25 19:30:26 +0100

    fixed all check functions and signup

--- FlaskWebArchiver/func.py
+++ FlaskWebArchiver/func.py
@@ -4,25 +4,51 @@ import os
 import sqlite3
 import hashlib
 
-def md5it(input):
+def md5hash(input):
     return hashlib.md5(input.encode()).hexdigest()
 
 def get_links(url): # the scraper
     soup = BeautifulSoup(requests.get(url=url).text)
     print(soup)
 
+
 def checkUserExists(username):
-    con = sqlite3.connect("../user.db")
+    #print(os.getcwd())
+    con = sqlite3.connect("user.db")
     cursor = con.cursor()    
-    cursor.execute(f"select 1 from userdata where username ='{username}';")
-    if cursor.rowcount():
-        return True # user exists
-    else:
-        return False # user doesnt exist
+    result = cursor.execute("select * from userdata where username =?", (username,)).fetchall()
+    try:
+        if len(result) > 0:
+            return True 
+        else:
+            return False
+    finally:        
+        cursor.close()
+        con.close()
+
 
 def checkPassword(username, password):
-    con = sqlite3.connect("../user.db")
-    cursor = con.cursor()    
-    cursor.execute(f"SELECT {username} FROM userdata WHERE username='{username}';")
+    phash = md5hash(password)
+    con = sqlite3.connect("user.db")
+    cursor = con.cursor()
+    result = cursor.execute("select * from userdata where username=? and phash=?", (username,phash)).fetchall()
+    if len(result) > 0:
+        return True
+    else:
+        return False
+        
 
-checkUserExists("test")
+def signup(username, password,email):
+    phash = md5hash(password)
+    con = sqlite3.connect("user.db")
+    cursor = con.cursor()
+    result = cursor.execute("INSERT INTO userdata (username,phash,email) VALUES (?,?,?)", [username,phash,email])
+    cursor.close()
+    con.commit()
+    con.close()
+    return f"user {username} registered"
+
+
+print(signup("admin","secret_password","admin@website.com"))
+print(checkUserExists("admin"))
+print(checkPassword("admin", "secret_password"))
