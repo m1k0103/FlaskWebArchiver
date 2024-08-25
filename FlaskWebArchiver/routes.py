5c3e330e3989eefe31a40cb3ae6a568fa92ad1d4
mikok <84585119+miko1034@users.noreply.github.com>
2024-08-25 13:40:52 +0100

    finished login and logout route. started signup

--- FlaskWebArchiver/routes.py
+++ FlaskWebArchiver/routes.py
@@ -1,16 +1,20 @@
-from flask import Flask, request, render_template, jsonify, redirect
+from flask import Flask, request, render_template, url_for, jsonify, redirect, session
 import os, sqlite3
 from markupsafe import escape
-from FlaskWebArchiver.func import checkDetails, cookieGen
+from FlaskWebArchiver.func import checkPassword
 
 app = Flask(__name__)
+app.secret_key = b"_test5#5#5[2#2#156kzz'xw]gsd_a"
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/signup")
+@app.route("/signup",methods=["GET","POST"])
 def signup():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
     return render_template("signup.html")
 
 @app.route("/login",methods=["GET", "POST"])
@@ -18,15 +22,22 @@ def login():
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
-        print(username,password)
-        #if checkDetails(username,password) == True:
-        #    cookie = cookieGen(username,password)
-        return redirect("dashboard.html")#,session=cookie)
+        if checkPassword(username, password) == True:
+            session["logged_in"] = True
+            return redirect(url_for("homepage"))
+        else:
+            return redirect(url_for("login"))
     else:
         return render_template("login.html")
 
+@app.route("/logout",methods=["GET"])
+def logout():
+    session.clear()
+    return redirect(url_for('homepage'))
+
 @app.route("/forgotpassword")
 def forgot_pass():
+    if session[]
     return render_template("forgotpassword.html")
 
 @app.route("/user/<string:username>")
@@ -52,6 +63,3 @@ def loading():
 @app.route("/debug")
 def test():
     return render_template("test.html")
-
-if __name__ == "__main__":
-    app.run()
