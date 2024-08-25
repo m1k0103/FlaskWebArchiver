3b0e9aee1540ca1f0c7bf462b13d30e49c9cb042
mikok <84585119+miko1034@users.noreply.github.com>
2024-08-25 13:40:04 +0100

    added md5it function

--- FlaskWebArchiver/func.py
+++ FlaskWebArchiver/func.py
@@ -2,13 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import sqlite3
+import hashlib
 
-def get_links(url):
+def md5it(input):
+    return hashlib.md5(input.encode()).hexdigest()
+
+def get_links(url): # the scraper
     soup = BeautifulSoup(requests.get(url=url).text)
     print(soup)
 
-def checkDetails():
-    pass
+def checkUserExists(username):
+    con = sqlite3.connect("../user.db")
+    cursor = con.cursor()    
+    cursor.execute(f"select 1 from userdata where username ='{username}';")
+    if cursor.rowcount():
+        return True # user exists
+    else:
+        return False # user doesnt exist
 
-def cookieGen():
-    pass
+def checkPassword(username, password):
+    con = sqlite3.connect("../user.db")
+    cursor = con.cursor()    
+    cursor.execute(f"SELECT {username} FROM userdata WHERE username='{username}';")
+
+checkUserExists("test")
