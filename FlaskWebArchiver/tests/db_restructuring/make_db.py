cc09880f3daf9033305f00d438e235cc56904cc0
mikok <84585119+miko1034@users.noreply.github.com>
2024-09-12 20:31:07 +0100

    rewrote check_password function for new db

--- FlaskWebArchiver/tests/db_restructuring/make_db.py
+++ FlaskWebArchiver/tests/db_restructuring/make_db.py
@@ -1,5 +1,8 @@
 import sqlite3
 import os
+import hashlib
+def md5hash(input):
+    return hashlib.md5(str(input).encode()).hexdigest()
 
 def db_make():
     db = open('./test.db', 'w+')
@@ -53,13 +56,13 @@ def insert_mock_data():
     cursor = con.cursor()
 
     cursor.execute("INSERT INTO stats(total_searches, total_saves) VALUES (0,0)")
-    cursor.execute(f"INSERT INTO userdata(username,phash,email,vercode, stat_id) VALUES ('larry','oigsakl','larry@mail.com',098776, {cursor.lastrowid})")
+    cursor.execute(f"INSERT INTO userdata(username,phash,email,vercode, stat_id) VALUES ('larry','{md5hash(1)}','larry@mail.com',098776, {cursor.lastrowid})")
 
     cursor.execute("INSERT INTO stats(total_searches, total_saves) VALUES (0,0)")
-    cursor.execute(f"INSERT INTO userdata(username,phash,email,vercode, stat_id) VALUES ('john','goasijmkopfgk','john@mail.com',123456, {cursor.lastrowid})")
+    cursor.execute(f"INSERT INTO userdata(username,phash,email,vercode, stat_id) VALUES ('john','{md5hash(2)}','john@mail.com',123456, {cursor.lastrowid})")
     
     cursor.execute("INSERT INTO stats(total_searches, total_saves) VALUES (0,0)")
-    cursor.execute(f"INSERT INTO userdata(username,phash,email,vercode, stat_id) VALUES ('bob','hashhashhasshhash','bob@mail.com',202020, {cursor.lastrowid})")
+    cursor.execute(f"INSERT INTO userdata(username,phash,email,vercode, stat_id) VALUES ('bob','{md5hash(3)}','bob@mail.com',202020, {cursor.lastrowid})")
     
     
     con.commit()
@@ -68,16 +71,31 @@ def insert_mock_data():
 def get_stats_by_username(username):
     con = sqlite3.connect("test.db")
     cursor = con.cursor()
-    #selects
+
+    # selects something im not too sure lol
     result = cursor.execute("SELECT userdata.stat_id, stats.total_searches, stats.total_saves FROM userdata JOIN stats ON (userdata.stat_id=stats.site_id) WHERE userdata.username=?", [username]).fetchall()[0]
     total_searches = result[1]
     total_saves = result[2]
 
+    con.close()
+
     print(total_searches, total_saves)
     return total_searches, total_saves
+
+def check_password(username,password):
+    con = sqlite3.connect("test.db")
+    cursor = con.cursor()
+    phash = md5hash(password)
+    if str(phash) == cursor.execute("SELECT phash FROM userdata WHERE username=?", [username]).fetchall()[0][0]:
+        con.close()
+        return True
+    else:
+        con.close()
+        return False 
 
 
 
 #db_make()
 #insert_mock_data()
-get_stats_by_username("bob")
+#get_stats_by_username("bob")
+print(check_password("bob",3))
